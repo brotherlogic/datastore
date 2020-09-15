b359f48a840885a19a5912ca2793b6919a101364
Simon Tucker <brotherlogic@gmail.com>
2020-09-15 16:18:30 -0700

    Fixed for selected consensus.

--- datastoreutils_test.go
+++ datastoreutils_test.go
@@ -2,6 +2,18 @@ package main
 
 import "testing"
 
+func TestMax(t *testing.T) {
+	if max(int32(12), 4) != int32(12) {
+		t.Errorf("Bad max")
+	}
+}
+
+func TestMin(t *testing.T) {
+	if min(int32(12), 4) != 4 {
+		t.Errorf("Bad min")
+	}
+}
+
 func TestFullExtract(t *testing.T) {
 	dir, file := extractFilename("this/is")
 	if dir != "this" || file != "is" {
